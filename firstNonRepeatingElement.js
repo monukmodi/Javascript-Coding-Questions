6a42a0406bc0b94b9c16ab2261f564470fb0dcb2
monukmodi <monukr2012@gmail.com>
2022-08-07 18:34:18 +0530

    FirstNonRepeatingELement

---
+++ firstNonRepeatingElement.js
@@ -0,0 +1,24 @@
+function firstNonRepeatingElement(str){
+    const obj ={}
+    for(let elem of str){
+        if(obj[elem]){
+            obj[elem] = obj[elem] + 1
+        }
+        else{
+            obj[elem]= 1
+        }
+        // obj[elem] = obj[elem] ? obj[elem] + 1 : 1
+    }
+    console.log("hii",obj)
+
+    for(let elem in obj){
+        if(obj[elem] === 1) {
+            return elem
+        }
+    }
+
+    return -1
+}
+
+var str = "1122343599787"
+console.log(firstNonRepeatingElement(str))
